db0b944820e6a253debbf89e8522174960026da2
Will Lu <zeren828@gmail.com>
2017-06-03 17:20:05 +0800

    delete

--- application/controllers/Homes.php
+++ application/controllers/Homes.php
@@ -3,11 +3,6 @@ defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
 class Homes extends CI_Controller {
 	private $data_view;
 	public function index() {
-		try {
-			// 套版
-			$this->load->view ( 'Vidol/include/html5', $this->data_view );
-		} catch ( Exception $e ) {
-			show_error ( $e->getMessage () . ' --- ' . $e->getTraceAsString () );
-		}
+		show_404();
 	}
 }
